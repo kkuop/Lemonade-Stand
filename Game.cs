6aa551cffa57c949d732b609cc81cf72dfea9a50
kkuop <60794860+kkuop@users.noreply.github.com>
2020-02-14 09:59:47 -0600

    added method for DisplayRules and HowManyPlayers

--- Game.cs
+++ Game.cs
@@ -8,5 +8,46 @@ namespace LemonadeStand_3DayStarter
 {
     class Game
     {
+        //member vars
+        int howManyPlayers;
+        //constructor
+
+        //member methods
+        public void StartGame()
+        {
+            //Here is the best case for single responsibility (each method has its own responsibility as we run through the entire game)
+            //Display rules
+            DisplayRules();
+            //How many people will be playing?
+            HowManyPlayers();
+            //Choose duration (7,14,21,28)
+            //Display weather report
+            //Get user input (how many glasses, what will be the price)
+            //Crunch the numbers
+            //Display report
+            //Loop back through starting from displaying the weather report
+        }
+        private void DisplayRules()
+        {
+            Console.WriteLine("Welcome to Lemonade Stand!");
+            Console.WriteLine("How to play:");
+        }
+        private void HowManyPlayers()
+        {
+            Console.WriteLine("How many players?");
+            try
+            {
+                howManyPlayers = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("That is not a number... try again!");
+                HowManyPlayers();
+            }
+            for (int i=0;i<howManyPlayers;i++)
+            {
+                new Player();
+            }
+        }
     }
 }
